61c201b01e39a1a99bbed458df0812549636941a
Giuseppe <mazzeogiu@gmail.com>
2021-05-05 16:16:28 +0200

    Fix error JOptionPane

--- src/main/java/fr/eql/al35/injection/InjectionApplication.java
+++ src/main/java/fr/eql/al35/injection/InjectionApplication.java
@@ -2,12 +2,20 @@ package fr.eql.al35.injection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class InjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InjectionApplication.class, args);
+		//SpringApplication.run(InjectionApplication.class, args);
+		
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(InjectionApplication.class);
+
+		builder.headless(false);
+
+		ConfigurableApplicationContext context = builder.run(args);
 	}
 
 }
--- src/main/java/fr/eql/al35/injection/gui/AfficheurGraphiqhe.java
+++ src/main/java/fr/eql/al35/injection/gui/AfficheurGraphiqhe.java
@@ -4,11 +4,12 @@ import javax.swing.JOptionPane;
 
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class AfficheurGraphiqhe implements Afficher {
 
 	@Override
 	public void afficher(String message) {
+		//le pachet javax.swing é optionale nelle versioni superiori a java 8
 		JOptionPane.showMessageDialog(null, message);
 
 	}
